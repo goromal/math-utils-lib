26c4bab749b24c6eb92fd7930cdc891c14b141b2
Andrew Torgesen <andrew.torgesen@gmail.com>
2020-04-11 22:36:20 -0400

    modify RK4 for Eigen-only types

--- include/math-utils-lib/modeling.h
+++ include/math-utils-lib/modeling.h
@@ -131,12 +131,13 @@ inline T wrap_angle_npi2pi(T theta)
 // ===========================================================================================================
 // Fourth order Runge-Kutta integration ======================================================================
 // ===========================================================================================================
+// WARNING: EIGEN-ONLY TEMPLATES!
 
-template <typename xT, typename uT>
+template <typename xT>
 class RK4
 {
 private:
-    boost::function<void(xT&, xT&, const uT&)> f_;
+    boost::function<void(Ref<VectorXd>, const Ref<const VectorXd>&, const Ref<const VectorXd>&)> f_;
     xT k1_;
     xT k2_;
     xT k3_;
@@ -147,29 +148,29 @@ private:
     bool initialized_;
 public:
     RK4() : initialized_(false) {}
-    RK4(void (*f)(xT&, xT&, const uT&))
+    RK4(void (*f)(Ref<VectorXd>, const Ref<const VectorXd>&, const Ref<const VectorXd>&))
     {
         f_ = boost::bind(f, _1, _2, _3);
         initialized_ = true;
     }
     template <class C>
-    RK4(void (C::*f)(xT&, xT&, const uT&), C *obj)
+    RK4(void (C::*f)(Ref<VectorXd>, const Ref<const VectorXd>&, const Ref<const VectorXd>&), C *obj)
     {
         f_ = boost::bind(f, obj, _1, _2, _3);
         initialized_ = true;
     }
-    void setDynamics(void (*f)(xT&, xT&, const uT&))
+    void setDynamics(void (*f)(Ref<VectorXd>, const Ref<const VectorXd>&, const Ref<const VectorXd>&))
     {
         f_ = boost::bind(f, _1, _2, _3);
         initialized_ = true;
     }
     template <class C>
-    void setDynamics(void (C::*f)(xT&, xT&, const uT&), C *obj)
+    void setDynamics(void (C::*f)(Ref<VectorXd>, const Ref<const VectorXd>&, const Ref<const VectorXd>&), C *obj)
     {
         f_ = boost::bind(f, obj, _1, _2, _3);
         initialized_ = true;
     }
-    void run(xT &x, const uT &u, const double &dt)
+    void run(Ref<VectorXd> x, const Ref<const VectorXd> &u, const double &dt)
     {
         if (initialized_)
         {
@@ -198,100 +199,100 @@ public:
 // ===========================================================================================================
 
 // TODO: initialization and relinearization methods that access the state space matrices directly ++++
-class LTIModelSISO
-{
-private:
-    bool initialized_;
-    MatrixXd A_;
-    MatrixXd B_;
-    MatrixXd C_;
-    MatrixXd D_;
-    MatrixXd x_;
-    RK4<MatrixXd, double> rk4_;
-    void createStateSpaceDenNum(MatrixXd &x0, const MatrixXd &alpha_vals, const MatrixXd &beta_vals)
-    {
-        if (alpha_vals.cols() == 1 && beta_vals.cols() == 1 &&
-                alpha_vals.rows() >= beta_vals.rows() && x0.rows() == alpha_vals.rows() - 1)
-        {
-            rk4_.setDynamics(&LTIModelSISO::f, this);
-            x_.resize(x0.rows(), 1);
-            x_ = x0;
+//class LTIModelSISO
+//{
+//private:
+//    bool initialized_;
+//    MatrixXd A_;
+//    MatrixXd B_;
+//    MatrixXd C_;
+//    MatrixXd D_;
+//    MatrixXd x_;
+//    RK4<MatrixXd, double> rk4_;
+//    void createStateSpaceDenNum(MatrixXd &x0, const MatrixXd &alpha_vals, const MatrixXd &beta_vals)
+//    {
+//        if (alpha_vals.cols() == 1 && beta_vals.cols() == 1 &&
+//                alpha_vals.rows() >= beta_vals.rows() && x0.rows() == alpha_vals.rows() - 1)
+//        {
+//            rk4_.setDynamics(&LTIModelSISO::f, this);
+//            x_.resize(x0.rows(), 1);
+//            x_ = x0;
 
-            reLinearizeDenNum(alpha_vals, beta_vals);
-        }
-    }
-    void f(MatrixXd &x_dot, MatrixXd &x, const double &u)
-    {
-        x_dot = A_ * x + B_ * u;
-    }
-public:
-    LTIModelSISO() : initialized_(false) {}
-    LTIModelSISO(MatrixXd &x0, const MatrixXd &alpha_vals, const MatrixXd &beta_vals)
-    {
-        createStateSpaceDenNum(x0, alpha_vals, beta_vals);
-    }
-    void setCoefficients(MatrixXd &x0, const MatrixXd &alpha_vals, const MatrixXd &beta_vals)
-    {
-        createStateSpaceDenNum(x0, alpha_vals, beta_vals);
-    }
-    void reLinearizeDenNum(const MatrixXd &alpha_vals, const MatrixXd &beta_vals)
-    {
-        if (alpha_vals.cols() == 1 && beta_vals.cols() == 1 && alpha_vals.rows() >= beta_vals.rows())
-        {
-            int n = alpha_vals.rows() - 1;
-            int m = beta_vals.rows() - 1;
-            MatrixXd beta_vals_buffered(n+1, 1);
-            for (int i = 0; i <= n; i++)
-            {
-                if (i <= m)
-                    beta_vals_buffered(i, 0) = beta_vals(i, 0);
-                else
-                    beta_vals_buffered(i, 0) = 0.0;
-            }
-            A_.resize(n, n);
-            B_.resize(n, 1);
-            C_.resize(1, n);
-            D_.resize(1, 1);
+//            reLinearizeDenNum(alpha_vals, beta_vals);
+//        }
+//    }
+//    void f(MatrixXd &x_dot, MatrixXd &x, const double &u)
+//    {
+//        x_dot = A_ * x + B_ * u;
+//    }
+//public:
+//    LTIModelSISO() : initialized_(false) {}
+//    LTIModelSISO(MatrixXd &x0, const MatrixXd &alpha_vals, const MatrixXd &beta_vals)
+//    {
+//        createStateSpaceDenNum(x0, alpha_vals, beta_vals);
+//    }
+//    void setCoefficients(MatrixXd &x0, const MatrixXd &alpha_vals, const MatrixXd &beta_vals)
+//    {
+//        createStateSpaceDenNum(x0, alpha_vals, beta_vals);
+//    }
+//    void reLinearizeDenNum(const MatrixXd &alpha_vals, const MatrixXd &beta_vals)
+//    {
+//        if (alpha_vals.cols() == 1 && beta_vals.cols() == 1 && alpha_vals.rows() >= beta_vals.rows())
+//        {
+//            int n = alpha_vals.rows() - 1;
+//            int m = beta_vals.rows() - 1;
+//            MatrixXd beta_vals_buffered(n+1, 1);
+//            for (int i = 0; i <= n; i++)
+//            {
+//                if (i <= m)
+//                    beta_vals_buffered(i, 0) = beta_vals(i, 0);
+//                else
+//                    beta_vals_buffered(i, 0) = 0.0;
+//            }
+//            A_.resize(n, n);
+//            B_.resize(n, 1);
+//            C_.resize(1, n);
+//            D_.resize(1, 1);
 
-            D_(0, 0) = beta_vals_buffered(n, 0) / alpha_vals(n, 0);
-            B_.setZero();
-            B_(n-1, 0) = 1.0 / alpha_vals(n, 0);
-            A_.setZero();
-            for (int i = 0; i < n; i++)
-            {
-                C_(0, i) = beta_vals_buffered(i, 0) - beta_vals_buffered(n, 0) * alpha_vals(i, 0) / alpha_vals(n, 0);
-                A_(n-1, i) = -alpha_vals(i, 0) / alpha_vals(n, 0);
-            }
-            for (int i = 1; i < n; i++)
-                A_(i-1, i) = 1.0;
+//            D_(0, 0) = beta_vals_buffered(n, 0) / alpha_vals(n, 0);
+//            B_.setZero();
+//            B_(n-1, 0) = 1.0 / alpha_vals(n, 0);
+//            A_.setZero();
+//            for (int i = 0; i < n; i++)
+//            {
+//                C_(0, i) = beta_vals_buffered(i, 0) - beta_vals_buffered(n, 0) * alpha_vals(i, 0) / alpha_vals(n, 0);
+//                A_(n-1, i) = -alpha_vals(i, 0) / alpha_vals(n, 0);
+//            }
+//            for (int i = 1; i < n; i++)
+//                A_(i-1, i) = 1.0;
 
-            initialized_ = true;
-        }
-        else
-            initialized_ = false;
-    }
-    double run(const double &u, const double &dt)
-    {
-        if (initialized_)
-        {
-            rk4_.run(x_, u, dt);
-            return (C_ * x_ + D_ * u)(0,0);
-        }
-        else
-            return 0.0;
-    }
-    inline MatrixXd A() const { return A_; }
-    inline MatrixXd B() const { return B_; }
-    inline MatrixXd C() const { return C_; }
-    inline MatrixXd D() const { return D_; }
-    inline MatrixXd x() const { return x_; }
-};
+//            initialized_ = true;
+//        }
+//        else
+//            initialized_ = false;
+//    }
+//    double run(const double &u, const double &dt)
+//    {
+//        if (initialized_)
+//        {
+//            rk4_.run(x_, u, dt);
+//            return (C_ * x_ + D_ * u)(0,0);
+//        }
+//        else
+//            return 0.0;
+//    }
+//    inline MatrixXd A() const { return A_; }
+//    inline MatrixXd B() const { return B_; }
+//    inline MatrixXd C() const { return C_; }
+//    inline MatrixXd D() const { return D_; }
+//    inline MatrixXd x() const { return x_; }
+//};
 
-inline std::ostream& operator<< (std::ostream& os, const LTIModelSISO& lms)
-{
-    os << "SISO-LTI System:\nA =\n" << lms.A() << "\nB =\n" << lms.B() << "\nC =\n" << lms.C()
-       << "\nD =\n" << lms.D();
-    return os;
-}
+//inline std::ostream& operator<< (std::ostream& os, const LTIModelSISO& lms)
+//{
+//    os << "SISO-LTI System:\nA =\n" << lms.A() << "\nB =\n" << lms.B() << "\nC =\n" << lms.C()
+//       << "\nD =\n" << lms.D();
+//    return os;
+//}
 
 } // end namespace modeling
